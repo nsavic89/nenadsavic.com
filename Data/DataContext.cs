40b6d30d684140256e0f62f9dd312d82e228e8cd
Nenad Savic <nenadschem@gmail.com>
2023-02-07 22:11:11 +0100

    Database - started

---
+++ Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using nenadsavic.com.Models;
+
+namespace nenadsavic.com.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public string DbPath { get; }
+
+        public DataContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "database.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using nenadsavic.com.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
